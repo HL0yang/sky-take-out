59d716ea02dbb4345681e5b75849cec985bb0bfd
HL0yang <batss127@gmail.com>
2023-10-13 16:35:54 +0800

    苍穹外卖新增员工和分页查询

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
  */
@@ -22,6 +24,22 @@ public class GlobalExceptionHandler {
     public Result exceptionHandler(BaseException ex){
         log.error("异常信息：{}", ex.getMessage());
         return Result.error(ex.getMessage());
+    }
+
+    //java.sql.SQLIntegrityConstraintViolationException: Duplicate entry '李四' for key 'employee.idx_username'
+
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        log.info("异常信息：{}",ex.getMessage());
+        if(ex.getMessage().contains("Duplicate entry")){
+            String message = ex.getMessage();
+            String[] s = message.split(" ");
+            String err = "用户"+s[2]+"已存在";
+            return Result.error(err);
+        }
+        else {
+            return Result.error("未知错误！");
+        }
     }
 
 }
